eb8a3a4fb45738701ca77c8f264664727469edc2
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-09-05 16:51:10 +0530

    Added players between 2 to 4

--- src/com/bridgelabz/deckofcards/DeckOfCards.java
+++ src/com/bridgelabz/deckofcards/DeckOfCards.java
@@ -1,15 +1,13 @@
 package com.bridgelabz.deckofcards;
 
-import org.w3c.dom.ls.LSOutput;
-
-import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class DeckOfCards {
     static String[] suits = new String[]{"Clubs", "Hearts", "Diamond", "Spades"};
     static String[] ranks = new String[]{"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
     static String[][] deckOfCards = new String[4][13];
-
+    static Scanner sc = new Scanner(System.in);
     public void initializeCards() {
         for (int i = 0; i < suits.length; i++) {
             for (int j = 0; j < ranks.length; j++) {
@@ -25,4 +23,16 @@ public class DeckOfCards {
             System.out.println(Arrays.toString(card));
         }
     }
-}
+
+    void addPlayers(){
+        int numOfPlayers;
+        do{
+            System.out.println("Enter number of players (2 to 4 ): ");
+            numOfPlayers = sc.nextInt();
+        }while (numOfPlayers < 2 || numOfPlayers > 4);
+            for(int i = 0; i < numOfPlayers; i++){
+                Player.addPlayer(new Player());
+            }
+            System.out.println(numOfPlayers+" players will play the game: ");
+        }
+    }
